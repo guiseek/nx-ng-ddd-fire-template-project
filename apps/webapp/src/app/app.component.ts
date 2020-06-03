40599870dd6c7ead9c0f6655e9b5bdaafa27dfe4
Gui Seek <guiseek@gmail.com>
2020-06-03 19:30:05 -0300

    produtos

--- apps/webapp/src/app/app.component.ts
+++ apps/webapp/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Message } from '@dekao/api-interfaces';
 
 
 @Component({
@@ -9,6 +8,6 @@ import { Message } from '@dekao/api-interfaces';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
+  // hello$ = this.http.get<Message>('/api/hello');
   constructor(private http: HttpClient) { }
 }
